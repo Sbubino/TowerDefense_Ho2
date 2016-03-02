32b1568df9842cefbee451d4302f7ba68ae09fb8
Alessandro Morello <ale.morello94@gmail.com>
2016-03-02 13:13:06 +0100

    Aggiunto movimento a nemici
    aggiunto e testato movimento nemici su piastrelle camminabili

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+    public LayerMask m_Mask;
+   
+    
+    public float Speed;
+    public float life;
+    public float banishmentCost;
+
+    Vector3[] tilePosition;
+    Vector3 nextTile;
+    Vector3 currentTile;
+    Vector3 lastTile;
+
+    void Awake()
+    {
+        tilePosition = new Vector3[2];
+        nextTile = transform.position;
+        
+    }
+
+
+    void Start()
+    {
+        
+        
+        StartCoroutine("TakeTilePosition");
+        
+    }
+
+
+
+
+    void  SetNextTile()
+    {
+        for(int i = 0; i<tilePosition.Length; i++)
+        {
+            if(tilePosition[i] != Vector3.zero && tilePosition[i] != lastTile)
+            {
+                nextTile = tilePosition[i];
+            }
+                
+        }
+
+       
+    }
+
+    IEnumerator TakeTilePosition()
+    {
+        
+        while (Vector3.Distance(transform.position, nextTile) >= 0 && Vector3.Distance(transform.position, nextTile) < 0.2f)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(), 0f, m_Mask, 0f, 2f);
+
+            if (hit.collider != null)
+            {
+               
+
+                Tile temp = hit.transform.gameObject.GetComponent<Tile>();
+
+                tilePosition = temp.GetPositionNextTile();
+
+                lastTile = currentTile;
+
+                currentTile = nextTile;
+
+               
+
+                SetNextTile();
+
+               
+
+              
+
+                Move();
+
+                yield return null;
+            }
+
+
+            while (Vector3.Distance(transform.position, nextTile) >= 0.2f)
+            {
+            
+                Move();
+               
+                yield return null;
+            }
+
+        }
+
+     
+
+        
+    }
+
+
+
+    void Move()
+    {
+        
+
+        transform.position =  Vector2.Lerp(transform.position, nextTile, Speed * Time.deltaTime);
+    }
+
+    public void DebugPositionTile()
+    {
+        for (int i = 0; i < tilePosition.Length; i++)
+        {
+            if (tilePosition[i] != null)
+            {
+                Debug.Log("Piastrella n : " + i + "  posizione : " + tilePosition[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Script/Tile.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+   
+
+    Vector3 nucleus;
+    Vector3[] positionNextTile;
+    RaycastHit2D[] nextTile;
+    LayerMask walkableTileMask;
+
+
+    void Awake()
+    {
+        walkableTileMask = 1 << 8;
+        SetNextTilePosition();
+
+
+    }
+
+   
+
+    
+    void SetNextTilePosition()
+    {
+        nextTile = Physics2D.BoxCastAll(transform.position, new Vector2(transform.localScale.x, transform.localScale.y), 0f, new Vector2(), 0f, walkableTileMask);
+
+        positionNextTile = new Vector3[nextTile.Length];
+        
+       
+
+        for (int i =  0; i< nextTile.Length; i++)
+        {
+            if (nextTile[i].collider.gameObject != this.gameObject)
+            {
+                if (Vector3.Distance(transform.position, nextTile[i].transform.position) < 1.3)
+                {
+                    for (int j = 0; j < positionNextTile.Length; j++)
+                    {
+
+                        if (positionNextTile[j] == Vector3.zero)
+                        {
+                            positionNextTile[j] = nextTile[i].transform.position;
+                            break;
+                        }
+
+                    }
+                }
+            }
+         }
+     }
+
+
+
+    public void DebugPositionTile()
+    {
+        for(int i = 0; i< positionNextTile.Length; i++)
+        {
+            if(positionNextTile[i] != null)
+            {
+                //Debug.Log("Piastrella n : " + i + "posizione : " + GetPositionNextTile()[i]);
+            }
+        }
+    }
+
+   
+    public Vector3[] GetPositionNextTile()
+    {
+            
+        return positionNextTile;
+    }
+
+
+
+}
